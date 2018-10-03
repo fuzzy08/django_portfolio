9b33e785da881187614843ffe829aa3d47a1f9b9
fuzzy8 <fuzzylittlemanpeach8@gmail.com>
2018-10-03 19:32:03 -0400

    added home page and jobs functionality

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+#create a blog model
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/')
+
+
+#add the blog app to the settings
+
+#create a migration
+
+#migrate
+
+#add to the admin
