36331baa526e54bf4c45e429801326da55e28d1d
Zach <31185413+fuzzylittlemanpeach8@users.noreply.github.com>
2018-10-04 23:11:24 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'Synchronized18',
+        'PASSWORD': 'put-password-here',
         'HOST': 'localhost',
         'PORT': '5432',
     }
